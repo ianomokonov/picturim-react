04ea062c66b510e1076cb108a421fc517193b3dc
Ivan Nomokonov <nomokonov.vana@yandex.ru>
2022-03-07 11:25:01 +0300

    prettier

---
+++ .eslintrc.js
@@ -0,0 +1,62 @@
+module.exports = {
+    env: {
+        browser: true,
+        es6: true,
+    },
+    extends: [
+        'eslint:recommended',
+        'plugin:react/recommended',
+        'plugin:react-hooks/recommended',
+        'plugin:prettier/recommended',
+        'plugin:jsx-a11y/strict',
+    ],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
+        },
+        ecmaVersion: 2018,
+        sourceType: 'module',
+    },
+    plugins: ['react', 'jsx-a11y', '@typescript-eslint'],
+    rules: {
+        'react-hooks/exhaustive-deps': 'error',
+        'react/react-in-jsx-scope': 'off',
+        'no-var': 'error',
+        'brace-style': 'error',
+        'prefer-template': 'error',
+        radix: 'error',
+        'space-before-blocks': 'error',
+        'import/prefer-default-export': 'off',
+        'jsx-a11y/label-has-for': [
+            'error',
+            {
+                required: {
+                    some: ['nesting', 'id'],
+                },
+            },
+        ],
+        'prettier/prettier': [
+            'error',
+            {
+                endOfLine: 'auto',
+            },
+        ],
+        'no-unused-vars': 'error',
+    },
+    overrides: [
+        {
+            files: [
+                '**/*.test.js',
+                '**/*.test.jsx',
+                '**/*.test.tsx',
+                '**/*.spec.js',
+                '**/*.spec.jsx',
+                '**/*.spec.tsx',
+            ],
+            env: {
+                jest: true,
+            },
+        },
+    ],
+};
