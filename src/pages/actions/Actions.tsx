2356a8b8b0a78201eb6503fea15c90fb9e12bcfb
Ivan Nomokonov <nomokonov.vana@yandex.ru>
2022-03-11 14:24:23 +0300

    старница действий

--- src/pages/actions/Actions.tsx
+++ src/pages/actions/Actions.tsx
@@ -1,5 +1,6 @@
 import { ReactElement, useContext, useEffect } from 'react';
 import { HeaderContext } from '../../contexts/HeaderContext';
+import styles from './Actions.module.scss';
 
 export const Actions = (): ReactElement => {
     const { setHeader } = useContext(HeaderContext);
@@ -7,5 +8,22 @@ export const Actions = (): ReactElement => {
         setHeader('Действия');
     });
 
-    return <div>Действия</div>;
+    return (
+        <div className={styles.actions}>
+            <div className={styles.action}>
+                <div
+                    className={styles['action__author-photo']}
+                    style={{ backgroundImage: 'url(/assets/ava.jpg)' }}
+                ></div>
+                <div className={styles.action__text}>
+                    <span className={styles['action__author-name']}>vanika_koma</span>
+                    <span className={styles.action__message}>нравится ваша публикация</span>
+                    <span className={styles['action__creat-date']}>1 нед.</span>
+                </div>
+                <div className={styles.action__target}>
+                    <img src="../assets/ava.jpg" alt="" />
+                </div>
+            </div>
+        </div>
+    );
 };
